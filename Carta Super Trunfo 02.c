d678dc97114f60a2a7b63b294b2b9f23174c89ef
deboracoutinho08 <149339288+deboracoutinho08@users.noreply.github.com>
2025-09-15 23:48:49 +0000

    Criei o printf

---
+++ Carta Super Trunfo 02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    //Carta 02//
+   printf("Carta 02 - Super Trunfo\n");
+
+    int populacao = 3343534;
+    int numero = 70;
+    float area = 4516.73;
+    float PIB = 2.948730;
+    char nome[30] = "Betim";
+    char codigo[30] = "A02";
+
+    printf("População: %d\n", populacao);
+    printf("Número de pontos turísticos: %d turistas\n", numero);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f trilhões de dólares\n", PIB);
+    printf("Código da carta: %s\n", codigo);
+    printf("Nome: %s\n", nome);
+
+    return 0;
+}
